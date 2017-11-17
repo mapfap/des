10ec6c58f781b33cb555fa6f601506a880a2241a
mapfap <mapfaps@gmail.com>
2017-11-17 23:59:46 +0700

    Handle JSON

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,9 @@
 class MenuController < ApplicationController
   def index
+    @items = MenuItem.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @items.to_json(only: [:name, :price])  }
+    end
   end
 end
